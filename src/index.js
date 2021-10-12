d5194fc576947e26235f0b27b4883f1ec36a77a3
psick22 <kgirok@gmail.com>
2021-10-12 12:24:42 +0900

    feature : add technical analysis widget

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-export { TradingViewIndicesWidget } from "./indices-widget"
-export { TradingViewStockChartWidget } from "./stock-chart-widget"
-export { TradingViewStockChartMiniWidget } from "./stock-chart-mini-widget"
-export { TradingViewStockInfoWidget } from "./stock-info-widget"
-export { TradingViewStockProfileWidget } from "./stock-profile-widget"
-export { TradingViewStockListWidget } from "./stock-list-widget"
-export { BarStyles, IntervalTypes, RangeTypes, Themes } from "./types"
+export { TradingViewIndicesWidget } from "./indices-widget";
+export { TradingViewStockChartWidget } from "./stock-chart-widget";
+export { TradingViewStockChartMiniWidget } from "./stock-chart-mini-widget";
+export { TradingViewStockInfoWidget } from "./stock-info-widget";
+export { TradingViewStockProfileWidget } from "./stock-profile-widget";
+export { TradingViewStockListWidget } from "./stock-list-widget";
+export { TradingViewTechnicalAnalysis } from "./technical-analysis";
+export { BarStyles, IntervalTypes, RangeTypes, Themes } from "./types";
---
+++ src/technical-analysis.js
@@ -0,0 +1,72 @@
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { IntervalTypes, Themes } from "./types";
+
+const SCRIPT_ID = "tradingview-technical-analysis-widget-script";
+const CONTAINER_ID = "tradingview-technical-analysis-widget";
+
+export class TradingViewTechnicalAnalysis extends PureComponent {
+  static propTypes = {
+    width: PropTypes.oneOf([PropTypes.number, "100%"]),
+    height: PropTypes.number,
+    interval: PropTypes.oneOf([
+      1,
+      3,
+      5,
+      15,
+      30,
+      60,
+      120,
+      180,
+      "1",
+      "3",
+      "5",
+      "15",
+      "30",
+      "60",
+      "120",
+      "180",
+      IntervalTypes.D,
+      IntervalTypes.W,
+    ]),
+    locale: PropTypes.string,
+    symbol: PropTypes.string.isRequired,
+    colorTheme: PropTypes.oneOf([Themes.LIGHT, Themes.DARK]),
+    isTransparent: PropTypes.bool,
+    showIntervalTabs: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    width: "100%",
+    height: "100%",
+    interval: 60,
+    locale: "en",
+    symbol: "BINANCE:BTCUSDT",
+    colorTheme: Themes.LIGHT,
+    isTransparent: true,
+    showIntervalTabs: true,
+  };
+
+  containerId = `${CONTAINER_ID}-${Math.random()}`;
+
+  componentDidMount = () => setTimeout(this.appendScript, 100);
+
+  appendScript = () => {
+    const script = document.createElement("script");
+
+    script.id = SCRIPT_ID;
+    script.type = "text/javascript";
+    script.async = true;
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js";
+    script.onload = onload;
+    script.innerHTML = JSON.stringify({
+      ...TradingViewStockListWidget.defaultProps,
+      ...this.props,
+    });
+
+    document.getElementById(this.containerId).appendChild(script);
+  };
+
+  render = () => <article id={this.containerId}></article>;
+}
