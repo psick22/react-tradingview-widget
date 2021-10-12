a4b64eb577ebc13c1861e744cf51af409ab4d27c
psick22 <kgirok@gmail.com>
2021-10-12 13:42:56 +0900

    mod ta widget

--- src/technical-analysis.js
+++ src/technical-analysis.js
@@ -7,6 +7,7 @@ const CONTAINER_ID = "tradingview-technical-analysis-widget";
 
 export class TradingViewTechnicalAnalysis extends PureComponent {
   static propTypes = {
+    autosize: PropTypes.bool,
     width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     interval: PropTypes.oneOf([
@@ -34,9 +35,11 @@ export class TradingViewTechnicalAnalysis extends PureComponent {
     colorTheme: PropTypes.oneOf([Themes.LIGHT, Themes.DARK]),
     isTransparent: PropTypes.bool,
     showIntervalTabs: PropTypes.bool,
+    widgetType: PropTypes.string,
   };
 
   static defaultProps = {
+    autosize: true,
     width: "100%",
     height: "100%",
     interval: 60,
@@ -45,28 +48,106 @@ export class TradingViewTechnicalAnalysis extends PureComponent {
     colorTheme: Themes.LIGHT,
     isTransparent: true,
     showIntervalTabs: true,
+    widgetType: "widget",
   };
 
   containerId = `${CONTAINER_ID}-${Math.random()}`;
 
-  componentDidMount = () => setTimeout(this.appendScript, 100);
+  componentDidMount = () => this.appendScript(this.initWidget);
 
-  appendScript = () => {
+  componentDidUpdate = () => {
+    this.cleanWidget();
+    this.initWidget();
+  };
+
+  canUseDOM = () =>
+    !!(
+      typeof window !== "undefined" &&
+      window.document &&
+      window.document.createElement
+    );
+
+  appendScript = (onload) => {
+    if (!this.canUseDOM()) {
+      onload();
+      return;
+    }
+
+    if (this.scriptExists()) {
+      /* global TradingView */
+      if (typeof TradingView === "undefined") {
+        this.updateOnloadListener(onload);
+        return;
+      }
+      onload();
+      return;
+    }
     const script = document.createElement("script");
-
     script.id = SCRIPT_ID;
     script.type = "text/javascript";
     script.async = true;
     script.src =
       "https://s3.tradingview.com/external-embedding/embed-widget-technical-analysis.js";
-    script.onload = onload;
-    script.innerHTML = JSON.stringify({
-      ...TradingViewTechnicalAnalysis.defaultProps,
-      ...this.props,
-    });
 
-    document.getElementById(this.containerId).appendChild(script);
+    script.onload = onload;
+    document.getElementsByTagName("head")[0].appendChild(script);
   };
 
-  render = () => <article id={this.containerId}></article>;
+  getScriptElement = () => document.getElementById(SCRIPT_ID);
+
+  scriptExists = () => this.getScriptElement() !== null;
+
+  updateOnloadListener = (onload) => {
+    const script = this.getScriptElement();
+    const oldOnload = script.onload;
+    return (script.onload = () => {
+      oldOnload();
+      onload();
+    });
+  };
+
+  initWidget = () => {
+    if (
+      typeof TradingView === "undefined" ||
+      !document.getElementById(this.containerId)
+    )
+      return;
+
+    const { widgetType, ...widgetConfig } = this.props;
+    const config = { ...widgetConfig, container_id: this.containerId };
+
+    if (config.autosize) {
+      delete config.width;
+      delete config.height;
+    }
+
+    if (typeof config.interval === "number") {
+      config.interval = config.interval.toString();
+    }
+
+    if (config.popup_width && typeof config.popup_width === "number") {
+      config.popup_width = config.popup_width.toString();
+    }
+
+    if (config.popup_height && typeof config.popup_height === "number") {
+      config.popup_height = config.popup_height.toString();
+    }
+
+    new TradingView[widgetType](config);
+  };
+
+  cleanWidget = () => {
+    if (!this.canUseDOM()) return;
+    document.getElementById(this.containerId).innerHTML = "";
+  };
+
+  getStyle = () => {
+    if (!this.props.autosize) return {};
+    return {
+      width: "100%",
+      height: "100%",
+    };
+  };
+
+  render = () => <article id={this.containerId} style={this.getStyle()} />;
 }
